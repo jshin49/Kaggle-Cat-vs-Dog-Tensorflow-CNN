10183db6ae04b845ffdea3e5fbc76b0d23a45602
Jay Shin <jshin49@gmail.com>
2017-05-04 23:11:15 +0800

    finished deep model with readme

--- configs.py
+++ configs.py
@@ -48,7 +48,7 @@ class DeepConfig(object):
     train_dir = './data/train'
     test_dir = './data/test'
     image_dir = './arrays/'
-    image_size = 64     # resize image to image_size*image_size
+    image_size = 150     # resize image to image_size*image_size
     channels = 3        # Channel Size
     valid_size = 1000
     train_size = 24000
