8f7b9b7dcd386dbc73a9c7043aaaa23cd1eb6061
Jay Shin <jshin49@gmail.com>
2017-05-05 23:49:16 +0800

    reduce epochs

--- configs.py
+++ configs.py
@@ -9,9 +9,9 @@ class SimpleConfig(object):
     # Hyperparameters
     lr = 0.00005        # Learning Rate
     l2 = 0.001          # L2 Loss Coefficient
-    dropout = 0.6       # Dropout Rate
+    dropout = 0.4       # Dropout Rate
     batch_size = 32     # SGD Batch Size
-    epochs = 20         # Number of Training Epochs
+    epochs = 15         # Number of Training Epochs
     threshold = 0.5     # Threshold for accurate classification
 
     # Data Processing
@@ -38,7 +38,7 @@ class DeepConfig(object):
     l2 = 0.001          # L2 Loss Coefficient
     dropout = 0.3       # Dropout Rate
     batch_size = 32     # SGD Batch Size
-    epochs = 30         # Number of Training Epochs
+    epochs = 16         # Number of Training Epochs
     threshold = 0.5     # Threshold for accurate classification
 
     # Data Processing
