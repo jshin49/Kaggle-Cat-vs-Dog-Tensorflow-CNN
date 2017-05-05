bb5c06c0de9ca238c7e698fa3f33d3aa6b4d6ebd
Jay Shin <jshin49@gmail.com>
2017-05-05 23:00:12 +0800

    remove dropout

--- deep_cnn_model.py
+++ deep_cnn_model.py
@@ -60,10 +60,6 @@ class DeepModel(object):
             activation=tf.nn.relu)
         pool1 = tf.layers.max_pooling2d(
             inputs=conv1, pool_size=[2, 2], strides=(2, 2))
-        pool1 = tf.layers.dropout(
-            inputs=pool1,
-            rate=self.config.dropout_cnn,
-            training=training)
 
         # Convolutional Layer #2
         conv2 = tf.layers.conv2d(
@@ -90,10 +86,6 @@ class DeepModel(object):
             activation=tf.nn.relu)
         pool2 = tf.layers.max_pooling2d(
             inputs=conv2, pool_size=[2, 2], strides=(2, 2))
-        pool2 = tf.layers.dropout(
-            inputs=pool2,
-            rate=self.config.dropout_cnn,
-            training=training)
 
         # Convolutional Layer #3
         conv3 = tf.layers.conv2d(
@@ -120,10 +112,6 @@ class DeepModel(object):
             activation=tf.nn.relu)
         pool3 = tf.layers.max_pooling2d(
             inputs=conv3, pool_size=[2, 2], strides=(2, 2))
-        pool3 = tf.layers.dropout(
-            inputs=pool3,
-            rate=self.config.dropout_cnn,
-            training=training)
 
         # return pool3
 
